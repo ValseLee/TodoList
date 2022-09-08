fc2d327fa91923cfa5e8e12d77a6252c9a4868f8
valseLee <sollleky72@gmail.com>
2022-09-08 22:47:50 +0900

    init: TodoApp Init

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by 이승준 on 2022/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
