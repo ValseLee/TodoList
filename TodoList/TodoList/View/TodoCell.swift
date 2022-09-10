bf36df0f3c72da2b59732df4c54ecac65d6af264
valseLee <sollleky72@gmail.com>
2022-09-10 21:54:13 +0900

    add: TodoCell, MemoViewController 생성

---
+++ TodoList/TodoList/View/TodoCell.swift
@@ -0,0 +1,23 @@
+//
+//  TodoCell.swift
+//  TodoList
+//
+//  Created by 이승준 on 2022/09/10.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
