587057e398b6fc827d74ce9819721a49b9372d37
valseLee <sollleky72@gmail.com>
2022-09-10 21:54:52 +0900

    feat: 기반코드 작성

--- TodoList/TodoList/Controllers/ViewController.swift
+++ TodoList/TodoList/Controllers/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+	
+	// MARK: LifeCycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		configTableView()
+		configNavBar()
 	}
 
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		tableView.reloadData()
+	}
+	
+	// MARK: Methods
+	private func configTableView() {
+		tableView.dataSource = self
+		tableView.separatorStyle = .none
+	}
+	
+	private func configNavBar() {
+		self.title = "Memo"
+		let plusBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(plusBtnTapped))
+		plusBtn.tintColor = .systemBlue
+		navigationItem.rightBarButtonItem = plusBtn
+	}
+	
+	@objc func plusBtnTapped() {
+		performSegue(withIdentifier: "TO_MEMO", sender: nil)
+	}
 
 }
 
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "TODO_CELL", for: indexPath)
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	
+}
+
+extension ViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		performSegue(withIdentifier: "TO_MEMO", sender: indexPath)
+	}
+	
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return UITableView.automaticDimension
+	}
+}
